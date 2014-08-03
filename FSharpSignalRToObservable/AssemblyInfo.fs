c202f637e2899fe66d5510274c077500f1574a0c
Marcus Kohnert <marcus.dev@live.com>
2014-08-03 13:24:39 +0200

    Initial project structure

---
+++ FSharpSignalRToObservable/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+module FSharpSignalRToObservable.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpSignalRToObservable")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+()
